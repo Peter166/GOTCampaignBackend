c7f3614348105f894f94d9a15f277154a3ea31fc
Peter Cygan <cyganpiotr@gmail.com>
2023-01-22 18:05:46 +0000

    changes

---
+++ src/main/java/com/gotcampaign/example/gotcampaign/GotCampaignApplication.java
@@ -0,0 +1,13 @@
+package com.gotcampaign.example.gotcampaign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotCampaignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotCampaignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gotcampaign/example/gotcampaign/GotCampaignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gotcampaign.example.gotcampaign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotCampaignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
